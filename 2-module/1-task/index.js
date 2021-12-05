6d5886312acf0e46ec43636c1a32f9e320d3b262
Valeriia-Kozlova21 <valeriia.k21@gmail.com>
2021-12-05 19:55:13 +0200

    2 module - 1 task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof(salaries[key]) == "number" && salaries[key] != NaN && isFinite(salaries[key])){
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
