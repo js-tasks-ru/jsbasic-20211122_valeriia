d83750f43e1b8c8f7b563d52853c9c06f740834c
Valeriia-Kozlova21 <valeriia.k21@gmail.com>
2022-01-16 14:27:50 +0200

    6 module - 1 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,16 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    rows = rows.map(row => `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button>X</button></td></tr>`).join('');
+    this.elem.innerHTML = `<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead><tbody>${rows}</tbody>`;
+
+    const buttons = this.elem.querySelectorAll('button');
+
+    for (let button of buttons) {
+      button.addEventListener('click', function () {
+        this.closest('tr').remove();
+      });
+    }
   }
 }
